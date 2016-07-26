a65da595e37cc263ff8ea1dd103239e1f6cc82a4
ezekg <ezekg@yahoo.com>
2016-07-26 15:19:34 -0500

    update default site dashboard

--- www/default/index.php
+++ www/default/index.php
@@ -1,1 +1,17 @@
-<?php phpinfo();
+<ul>
+  <li>
+    <a href="/phpinfo"><code>phpinfo</code></a>
+  </li>
+  <li>
+    <a href="/phpmyadmin"><code>phpmyadmin</code></a>
+  </li>
+  <li>
+    <a href="/phpmemcacheadmin"><code>phpmemcacheadmin</code></a>
+  </li>
+  <li>
+    <a href="/opcache-status"><code>opcache-status</code></a>
+  </li>
+  <li>
+    <a href="/webgrind"><code>webgrind</code></a>
+  </li>
+</ul>
