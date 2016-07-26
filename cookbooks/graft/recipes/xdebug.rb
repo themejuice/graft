7d3120b7d65378e7d662117e1b87e76679b9fda5
ezekg <ezekg@yahoo.com>
2016-07-26 11:23:59 -0500

    use symlink instead of copy

--- cookbooks/graft/recipes/xdebug.rb
+++ cookbooks/graft/recipes/xdebug.rb
@@ -6,7 +6,7 @@
 version = node["php"]["version"]
 command =
   if node["xdebug"]["enabled"]
-    "sudo cp /etc/php/#{version}/mods-available/xdebug.ini /etc/php/#{version}/apache2/conf.d/20-xdebug.ini"
+    "sudo ln -fs /etc/php/#{version}/mods-available/xdebug.ini /etc/php/#{version}/apache2/conf.d/20-xdebug.ini"
   else
     "sudo rm -f /etc/php/#{version}/apache2/conf.d/20-xdebug.ini"
   end
