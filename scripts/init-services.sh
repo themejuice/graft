6459889ac305fbbb39749717b9d07e40c1c4be13
ezekg <ezekg@yahoo.com>
2016-08-02 18:25:12 -0500

    use stop/start for mailcatcher service

--- scripts/init-services.sh
+++ scripts/init-services.sh
@@ -4,4 +4,5 @@
 sudo service apache2 restart
 sudo service mysql-default restart
 sudo service memcached restart
-sudo service mailcatcher restart 2> /dev/null
+sudo service mailcatcher stop 2> /dev/null
+sudo service mailcatcher start 2> /dev/null
