c5a0d4683b7d80f56e2c6edb54b6b387eeb16818
Marc Garnier <marc.garnier@redshooters.ch>
2016-11-16 09:39:22 +0100

    fix issue with db import script
    Existing databases was overridden each time the VM starts

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -19,16 +19,25 @@ cd /srv/database/backup/
 
 # Parse through each file in the directory and use the file name to
 # import the SQL file into the database of the same name
-sql_count=`ls -1 *.sql 2> /dev/null | wc -l`
-
-if [ $sql_count != "0" ]; then
-  for file in $(ls *.sql); do
-    db_name=${file%%.sql}
-    mysql_cmd='SHOW TABLES FROM `'$db_name'`' # Required to support hypens in database names
-    db_exist=`mysql -u root -proot --skip-column-names -e "$mysql_cmd" 2> /dev/null`
-    if [ "$?" == "0" ] && [ "$db_exist" != "" ]; then
-      mysql -u root -proot "$db_name" < "$db_name.sql" 2> /dev/null
-      echo "Imported backup database '$db_name'"
-    fi
-  done
+sql_count=`ls -1 *.sql 2>/dev/null | wc -l`
+if [ $sql_count != 0 ]; then
+	for file in $( ls *.sql ); do
+	db_name=${file%%.sql}
+	mysql_cmd='SHOW TABLES FROM `'$db_name'`' # Required to support hypens in database names
+	db_exist=`mysql -u root -proot --skip-column-names -e "$mysql_cmd" 2> /dev/null`
+	if [ "$?" != "0" ]; then
+		printf "  * Error - Create $db_name database via init-custom.sql before attempting import\n\n"
+	else
+		if [ "" == "$db_exist" ]; then
+			printf "mysql -u root -proot $db_name < $db_name.sql\n"
+			mysql -u root -proot $db_name < $db_name.sql 2> /dev/null
+			printf "  * Import of $db_name successful\n"
+		else
+			printf "  * Skipped import of $db_name - tables exist\n"
+		fi
+	fi
+	done
+	printf "Databases imported\n"
+else
+	printf "No custom databases to import\n"
 fi
