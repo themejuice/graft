0dd8e2d2b64862f68763068af3b12f2adc8606c7
ezekg <ezekg@yahoo.com>
2016-11-18 17:00:15 -0600

    clean up code style
    - closes #12

--- scripts/import-db.sh
+++ scripts/import-db.sh
@@ -21,23 +21,23 @@ cd /srv/database/backup/
 # import the SQL file into the database of the same name
 sql_count=`ls -1 *.sql 2>/dev/null | wc -l`
 if [ $sql_count != 0 ]; then
-	for file in $( ls *.sql ); do
-	db_name=${file%%.sql}
-	mysql_cmd='SHOW TABLES FROM `'$db_name'`' # Required to support hypens in database names
-	db_exist=`mysql -u root -proot --skip-column-names -e "$mysql_cmd" 2> /dev/null`
-	if [ "$?" != "0" ]; then
-		printf "  * Error - Create $db_name database via init-custom.sql before attempting import\n\n"
-	else
-		if [ "" == "$db_exist" ]; then
-			printf "mysql -u root -proot $db_name < $db_name.sql\n"
-			mysql -u root -proot $db_name < $db_name.sql 2> /dev/null
-			printf "  * Import of $db_name successful\n"
-		else
-			printf "  * Skipped import of $db_name - tables exist\n"
-		fi
-	fi
-	done
-	printf "Databases imported\n"
+  for file in $( ls *.sql ); do
+  db_name=${file%%.sql}
+  mysql_cmd='SHOW TABLES FROM `'$db_name'`' # Required to support hypens in database names
+  db_exist=`mysql -u root -proot --skip-column-names -e "$mysql_cmd" 2> /dev/null`
+  if [ "$?" != "0" ]; then
+    echo "Error: create $db_name database via init-custom.sql before attempting import"
+  else
+    if [ "" == "$db_exist" ]; then
+      echo "mysql -u root -proot $db_name < $db_name.sql"
+      mysql -u root -proot $db_name < $db_name.sql 2> /dev/null
+      echo "Import of $db_name successful"
+    else
+      echo "Skipped import of $db_name (tables exist)"
+    fi
+  fi
+  done
+  echo "Databases imported"
 else
-	printf "No custom databases to import\n"
+  echo "No databases to import"
 fi
